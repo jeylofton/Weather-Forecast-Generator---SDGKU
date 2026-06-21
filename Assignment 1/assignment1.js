2ab5adf4093824436905f7a3acca5f2c461f05dc
jeylofton <jeylofton@gmail.com>
2026-06-21 05:41:57 -0500

    First initial Code

---
+++ Assignment 1/assignment1.js
@@ -0,0 +1,24 @@
+const reportTitle = "DAILY WEATHER REPORT";
+const temperatureUnit = "°C";
+
+let city = "Panamá City";
+let temperature = 30;
+let condition = "Partly Cloudy";
+let recommendation = "Carry water and stay hydrated";
+
+console.log(reportTitle);
+console.log("City: " + city);
+console.log("Temperature: " + temperature + temperatureUnit);
+console.log("Condition: " + condition);
+console.log("Recommendation: " + recommendation);
+
+city = "David";
+temperature = 27;
+condition = "Rainy";
+recommendation = "Carry an umbrella";
+
+console.log(reportTitle);
+console.log("City: " + city);
+console.log("Temperature: " + temperature + temperatureUnit);
+console.log("Condition: " + condition);
+console.log("Recommendation: " + recommendation);
---
+++ script.js
@@ -0,0 +1,40 @@
+console. log("Variables!");
+
+var firstName = "Jey";
+var thisIsMyVariableCalledSomething = 10;
+console.log(firstName);
+console.log(thisIsMyVariableCalledSomething);
+
+// firstName = "Luis"
+console.log(firstName)
+
+let number = 10;
+
+number = 25;
+
+// Boolean
+let isStudent = false;
+
+console.log("My name is: " + firstName + "I'm " + number + " years old");
+
+// Aritmetic Operaitions
+let num1 = 5;
+let num2 = 3;
+
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+
+console.log("The sum is " + addition);
+console.log("The difference is " + subtraction);
+console.log("The product is " + multiplication);
+console.log("The quotient is " + division);
+
+// Constants
+const weekDays = 7;
+console.log(weekDays);
+
+// Prompt
+word = prompt("Type Something")
+console.log(word)
